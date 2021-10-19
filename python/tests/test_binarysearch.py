6f302ff406f08fc16b5684dca9e4e2e0c36a3fa0
ahmadalrasheed <alrasheedahmad97@gmail.com>
2021-10-19 09:40:02 +0300

    last edit on arraybinarysearch

--- python/tests/test_binarysearch.py
+++ python/tests/test_binarysearch.py
@@ -3,8 +3,8 @@ from code_challenges.BinarySearch.binary_search import binarySearch
 
 def test_binarySearch():
     arr=[4, 8, 15, 16, 23, 42]
-    x=15
-    expected=2
+    x=16
+    expected=3
     actual=binarySearch(arr, x)
     assert actual==expected
 
